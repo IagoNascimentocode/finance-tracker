481330073c31b8c63951655ef071fdb7982e7dec
Iago Nascimento <iago_ns@hotmail.com>
2025-01-14 23:05:09 -0300

    feat: Server is running

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,31 @@
-import { z } from "zod";
+import express, { NextFunction, Request, Response } from "express";
+// import AppDataSource from "./database/data-source";
+import "reflect-metadata";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import { router } from "./router";
+import { AppError } from "./utils/AppError";
 
-const userSchema = z.object({
-  name: z.string()
-    .min(3, { message: "O nome precisa de 3 caracteres" })
-    .transform(name => name.toLowerCase()),
-  age: z.number().min(18, { message: "A idade precisa ser maior ou igual a 18" }),
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+// AppDataSource.initialize().then(() => {
+//   console.log("Database connected");
+// })
+
+app.use(router)
+
+// @ts-ignore
+app.use((err: Error, _request: Request, response: Response, _next: NextFunction) => {
+
+  if (err instanceof AppError) {
+    return response.status(err.status).json({ status: err.status, message: err.message })
+  }
+  console.log(err)
+  return response.status(500).json({ status: "error", message: "Internal Server Error" })
+
 })
-
-type User = z.infer<typeof userSchema>
-
-function getUserInfo(user: User): void {
-  const { name, age } = userSchema.parse(user)
-
-  console.log(`Nome: ${name}, Idade: ${age}`)
-}
-
-getUserInfo({
-  name: "Iago",
-  age: 18
-})
+app.listen(3000, () => console.log("Server is running"));
